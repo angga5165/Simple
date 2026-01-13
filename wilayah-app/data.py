2d7a0b5b5c453941c3299a9e318daac4d00c48ef
angga5165 <154061942+angga5165@users.noreply.github.com>
2026-01-13 11:07:03 +0700

    Create data.py

---
+++ wilayah-app/data.py
@@ -0,0 +1,30 @@
+provinsi = {
+    1: "DKI Jakarta",
+    2: "Jawa Barat"
+}
+
+kabupaten = {
+    1: [{"id": 1, "nama": "Jakarta Selatan"}, {"id": 2, "nama": "Jakarta Timur"}],
+    2: [{"id": 3, "nama": "Bandung"}, {"id": 4, "nama": "Bekasi"}]
+}
+
+kecamatan = {
+    1: [{"id": 1, "nama": "Kebayoran Baru"}],
+    2: [{"id": 2, "nama": "Duren Sawit"}],
+    3: [{"id": 3, "nama": "Coblong"}],
+    4: [{"id": 4, "nama": "Bekasi Selatan"}]
+}
+
+kelurahan = {
+    1: [{"id": 1, "nama": "Gandaria"}],
+    2: [{"id": 2, "nama": "Pondok Bambu"}],
+    3: [{"id": 3, "nama": "Dago"}],
+    4: [{"id": 4, "nama": "Jakamulya"}]
+}
+
+usaha = {
+    1: ["Warung Makan Bu Ani", "Laundry Gandaria"],
+    2: ["Toko Sembako Jaya"],
+    3: ["Coffee Dago"],
+    4: ["Bengkel Motor Bekasi"]
+}
